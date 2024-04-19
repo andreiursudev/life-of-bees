b73a69edeefb38b17ada3a219379bd481f9722cb
MarianBastiurea <marian.bastiurea@gmail.com>
2024-04-19 17:56:21 +0100

    reverse mistake

--- src/main/java/com/marianbastiurea/lifeofbees/Environment.java
+++ src/main/java/com/marianbastiurea/lifeofbees/Environment.java
@@ -12,9 +12,16 @@ import com.marianbastiurea.lifeofbees.weather.Weather;
 public class Environment {
     private Weather weather;
 
+    private Square[][] terrain = new Square[100][100];
 
-    public Environment( Weather weather) {
+    public Environment(Square square, Weather weather) {
         this.weather = weather;
+        terrain[square.getX()][square.getY()] = square;
+    }
+
+
+    public Square[][] getTerrain() {
+        return terrain;
     }
 
     public Weather getWeather() {
