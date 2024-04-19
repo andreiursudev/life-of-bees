84c2944d800e683f4ede7599304b9ede9ea37886
MarianBastiurea <marian.bastiurea@gmail.com>
2024-04-19 17:54:37 +0100

    remove outdated code

--- src/main/java/com/marianbastiurea/lifeofbees/Environment.java
+++ src/main/java/com/marianbastiurea/lifeofbees/Environment.java
@@ -12,16 +12,9 @@ import com.marianbastiurea.lifeofbees.weather.Weather;
 public class Environment {
     private Weather weather;
 
-    private Square[][] terrain = new Square[100][100];
 
-    public Environment(Square square, Weather weather) {
+    public Environment( Weather weather) {
         this.weather = weather;
-        terrain[square.getX()][square.getY()] = square;
-    }
-
-
-    public Square[][] getTerrain() {
-        return terrain;
     }
 
     public Weather getWeather() {
