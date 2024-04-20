5c1107da421e36518a51371517c9acc717b2c2c3
MarianBastiurea <marian.bastiurea@gmail.com>
2024-04-20 19:07:05 +0100

    Create a test to check queen's age algorithm

--- src/test/java/com/marianbastiurea/lifeofbees/QueenAgeTest.java
+++ src/test/java/com/marianbastiurea/lifeofbees/QueenAgeTest.java
@@ -6,7 +6,7 @@ public class QueenAgeTest {
     @Test
     void given4AsQueenAgeAnd1AsOptionShouldReturn1(){
         int ageOfQueen=4;
-        String answer=Queen.ageOfQueenIndex(ageOfQueen);
+      //  String answer=Queen.ageOfQueenIndex(ageOfQueen);
         assertEquals(1,Queen.ageOfQueenIndex(ageOfQueen));
 
     }
