61ef3cd1e6cfa4ef775b0a371b9b6f5912ebee31
MarianBastiurea <marian.bastiurea@gmail.com>
2024-04-20 16:43:10 +0100

    add a scanner class
    add a scanner class to ask beekeeper to choose between to replace queen when it's 4 years old

--- src/main/java/com/marianbastiurea/lifeofbees/Queen.java
+++ src/main/java/com/marianbastiurea/lifeofbees/Queen.java
@@ -1,5 +1,5 @@
 package com.marianbastiurea.lifeofbees;
-
+import java.util.Scanner;
 /*
 When the honeybee colony senses that it needs a new queen, perhaps because the queen is ailing or is preparing to
 swarm, the worker bees will begin the process of raising new queen bees.
@@ -19,33 +19,63 @@ In the summer, she can lay up to 2000 eggs a day!
 A drone is a male bee. Unlike the female worker bee, a drone has no stinger.
  */
 
+import java.util.Scanner;
+
 public class Queen {
-    private int ageOfQueen=1;
+    private int ageOfQueen = 1;
 
     public int getAgeOfQueen() {
         return ageOfQueen;
     }
 
     public void makeEggs(Hive hive, Environment environment) {
-        int numberOfEggs = (int) (2000 */* honeyIndex * */environment.getWeather().getTemperature()* ageOfQueenIndex(ageOfQueen));
+        int numberOfEggs = (int) (2000 */* honeyIndex * */environment.getWeather().getTemperature() * ageOfQueenIndex(ageOfQueen));
 
-        hive.addEggs(numberOfEggs) ;
+        hive.addEggs(numberOfEggs);
     }
-    public double ageOfQueenIndex(int ageOfQueen) {
+
+    public static double ageOfQueenIndex(int ageOfQueen) {
         /* a queen lives 3-5 years. We will consider 1 years old for our queen. At age 5,
     beekeper will have to replace this queen with new one.Depending on age of queen will choose an fertility index
      between 0 and 1. When index is 0, queen is too old to lay eggs and she have to be replaced. Whenn index is 1,
      fertility of queen is at maximum and she can lay upon 2000 eggs daily
          */
+        Scanner scanner = new Scanner(System.in);
+        String answer;
+        boolean isValidAnswer = false;
 
         switch (ageOfQueen) {
-            case
-                    0, 1, 2, 3:
-                return  1;
+            case 0, 1, 2, 3:
+                return 1;
             case 4:
-                return  0.75;
+                System.out.println("""
+                        Your queen is 4 years old.
+                        You have to change hive's queen this year or next year at last.
+                        Please choose one option from bellow:
+                        1) You want to change this year?
+                        2) You want to let the bees to decide when it's right time to change?
+                        That means your hive population of bees will decrease and honey productivity will go down.
+                        """);
+                do {
+                    System.out.print("Your choice (1 or 2): ");
+                    answer = scanner.nextLine();
+
+                    if (answer.equals("1") || answer.equals("2")) {
+                        isValidAnswer = true;
+                    } else {
+                        System.out.println("Invalid input. Please enter '1' or '2'.");
+                    }
+                } while (!isValidAnswer);
+
+                System.out.println("Thank you for your response!");
+                scanner.close();
+                if(answer.equals("1")){
+                    ageOfQueen=0;
+                    return 1;
+                }else if (answer.equals("2")){
+                return 0.75;}
             case 5:
-                return  0.25;
+                return 0.25;
             default:
                 break;
         }
