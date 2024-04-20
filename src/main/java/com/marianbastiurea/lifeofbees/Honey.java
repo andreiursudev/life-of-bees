bba1de872f206ac51080411401f8a07ddbb515cb
MarianBastiurea <marian.bastiurea@gmail.com>
2024-04-20 08:11:36 +0100

    update honey class with honey types

--- src/main/java/com/marianbastiurea/lifeofbees/Honey.java
+++ src/main/java/com/marianbastiurea/lifeofbees/Honey.java
@@ -8,4 +8,42 @@ package com.marianbastiurea.lifeofbees;
  */
 
 public class Honey {
+    private String honeyType;
+
+    public static String getHoneyTypes(HarvestingMonths month, int dayOfMonth) {
+        String honeyType;
+        switch (month) {
+            case MARCH:
+                return honeyType="";
+            case APRIL:
+                if (dayOfMonth >= 1 && dayOfMonth <= 20) {
+                    return honeyType="Rapeseed";
+                } else if (dayOfMonth >= 21 && dayOfMonth <= 30) {
+                    return honeyType="WildFlower";
+                }
+                break;
+            case MAY:
+                if (dayOfMonth >= 1 && dayOfMonth <= 20) {
+                    return honeyType="Acacia";
+                }else if (dayOfMonth >= 21 && dayOfMonth <= 31) {
+                    return honeyType="FalseIndigo";
+                }
+                break;
+            case JUNE:
+                if (dayOfMonth >= 1 && dayOfMonth <= 20) {
+                    return honeyType="Linden";
+                }else if (dayOfMonth >= 21 && dayOfMonth <= 30) {
+                    return honeyType="WildFlower";
+                }
+                break;
+            case JULY:
+                return honeyType="SunFlower";
+            case AUGUST:
+                return honeyType="WildFlower";
+            default:
+                break;
+        }
+        return honeyType=""; // Default value if month and day combination doesn't match any condition
+    }
+
 }
