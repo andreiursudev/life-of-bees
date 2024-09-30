3ca5dcc6e15c48d619ec1dde64c069d7036be0c8
MarianBastiurea <marian.bastiurea@gmail.com>
2024-09-30 14:33:06 +0100

    create FormularStart .js for input credential

---
+++ frontend/bees-app/src/components/FormularStart.js
@@ -0,0 +1,64 @@
+import React from 'react';
+
+const FormularStart = ({ handleClose }) => {
+    return (
+        <div className="modal show" style={{ display: 'block' }}>
+            <div className="modal-dialog">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h4 className="modal-title">New Game</h4>
+                        <button type="button" className="btn-close" onClick={handleClose}></button>
+                    </div>
+                    <div className="modal-body">
+                        <form>
+                            <div className="row mb-3 align-items-center">
+                                <div className="col">
+                                    <label htmlFor="name" className="col-form-label">Name</label>
+                                </div>
+                                <div className="col">
+                                    <input type="text" className="form-control" id="name" name="name" />
+                                </div>
+                            </div>
+
+                            <div className="row mb-3 align-items-center">
+                                <div className="col">
+                                    <label htmlFor="location" className="col-form-label">Location</label>
+                                </div>
+                                <div className="col">
+                                    <input type="text" className="form-control" id="location" name="location" />
+                                </div>
+                            </div>
+
+                            <div className="row mb-3 align-items-center">
+                                <div className="col">
+                                    <label htmlFor="startDate" className="form-label">Start Date:</label>
+                                </div>
+                                <div className="col">
+                                    <input type="date" className="form-control" id="startDate" name="startDate" />
+                                </div>
+                            </div>
+
+                            <div className="row mb-3 align-items-center">
+                                <div className="col">
+                                    <label htmlFor="hives" className="col-form-label">Hives</label>
+                                    <span id="errorMessage" className="warning-message">(Values less than 5!)</span>
+                                </div>
+                                <div className="col">
+                                    <input type="number" id="numberInput" className="form-control" name="numberInput" min="0" max="5" required />
+                                </div>
+                            </div>
+                        </form>
+                        <button className="btn btn-secondary" onClick={() => window.location.href = 'gameView.html'}>Start</button>
+                        <button className="btn btn-warning" onClick={() => window.location.href = 'gameView.html'}>Random</button>
+                    </div>
+
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-danger" onClick={handleClose}>Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FormularStart;
