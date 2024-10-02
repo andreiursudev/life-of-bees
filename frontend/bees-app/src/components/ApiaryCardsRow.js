d1864c4652833358f58722dc33172b7ba5486ce2
MarianBastiurea <marian.bastiurea@gmail.com>
2024-10-02 21:43:01 +0100

    update BeesApiSErvice and ApiaryCardsRow

--- frontend/bees-app/src/components/ApiaryCardsRow.js
+++ frontend/bees-app/src/components/ApiaryCardsRow.js
@@ -1,53 +1,22 @@
-
 import React from 'react';
+import { getGame } from './BeesApiService';
 
 const ApiaryCardsRow = () => {
     return (
         <div className="row">
-            <div className="col-md-3">
-                <div className="card">
-                    <div className="card-body">
-                        <h4 className="card-title">Dacic Apiary</h4>
-                        <p>Location: Galati, Romania</p>
-                        <p>Hives:12</p>
-                        <p>Bees:156790</p>
-                        <p>Honey:5kg</p>
+            {getGame.map((apiary, index) => (
+                <div className="col-md-4" key={index}>
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">{apiary.gameName}</h5>
+                            <p className="card-text">Location: {apiary.location}</p>
+                            <p className="card-text">Hives: {apiary.hives}</p>
+                            <p className="card-text">Bees: {apiary.bees}</p>
+                            <p className="card-text">Honey: {apiary.honey}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="col-md-3">
-                <div className="card">
-                    <div className="card-body">
-                        <h4 className="card-title">Dacic Apiary</h4>
-                        <p>Location: Galati, Romania</p>
-                        <p>Hives:12</p>
-                        <p>Bees:156790</p>
-                        <p>Honey:5kg</p>
-                    </div>
-                </div>
-            </div>
-            <div className="col-md-3">
-                <div className="card">
-                    <div className="card-body">
-                        <h4 className="card-title">Dacic Apiary</h4>
-                        <p>Location: Galati, Romania</p>
-                        <p>Hives:12</p>
-                        <p>Bees:156790</p>
-                        <p>Honey:5kg</p>
-                    </div>
-                </div>
-            </div>
-            <div className="col-md-3">
-                <div className="card">
-                    <div className="card-body">
-                        <h4 className="card-title">Dacic Apiary</h4>
-                        <p>Location: Galati, Romania</p>
-                        <p>Hives:12</p>
-                        <p>Bees:156790</p>
-                        <p>Honey:5kg</p>
-                    </div>
-                </div>
-            </div>
+            ))}
         </div>
     );
 };
---
+++ frontend/bees-app/src/components/BeesApiService.js
@@ -0,0 +1,51 @@
+
+export const createGame
+    = [{
+        gameName: "Dacic Apiary",
+        location: "Sarmizegetusa, Romania",
+        startDate:"1-Apr-2023",
+        hives: "5",
+    }]
+
+    // BeesAppService.js
+export const getGame = [
+    {
+        gameName: "Stefan Cel Mare Apiary",
+        location: "Suceava, Romania",
+        hives: "50000000000000",
+        bees: "23456789",
+        honey: "76677"
+    },
+    {
+        gameName: "Vlad Tepes Apiary",
+        location: "Targoviste, Romania",
+        hives: "30000000000000",
+        bees: "12345678",
+        honey: "56677"
+    },
+    {
+        gameName: "Mihai Viteazul Apiary",
+        location: "Alba Iulia, Romania",
+        hives: "20000000000000",
+        bees: "34567890",
+        honey: "46677"
+    }
+];
+
+
+export const iterateGame
+    = [{
+        gameName: "Dacic Apiary",
+        NextDate:"8-Apr-2024"
+    }]
+
+
+export const SellHoney
+    = [{
+        rapeseedHoney: "50",
+        acaciaHoney: "40",
+        lindenHoney: "50.6",
+        wildFlowerHoney: "22.8",
+        sunFlowerHoney: "50.9",
+        falseIndigoFlowerHoney: "50",
+    }]
