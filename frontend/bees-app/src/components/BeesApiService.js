bbd1da60834cbe1d2160d9bbc56560759928e86b
MarianBastiurea <marian.bastiurea@gmail.com>
2024-10-03 12:49:10 +0100

    change name from getGames in showGames

--- frontend/bees-app/src/components/BeesApiService.js
+++ frontend/bees-app/src/components/BeesApiService.js
@@ -1,139 +1,140 @@
 
-export const createGame
-    = [{
-        gameName: "Dacic Apiary",
-        location: "Sarmizegetusa, Romania",
-        startDate: "1-Apr-2023",
-        hives: "5",
+    export const createGame
+        = [{
+            gameName: "Dacic Apiary",
+            location: "Sarmizegetusa, Romania",
+            startDate: "1-Apr-2023",
+            hives: "5",
+        }]
+
+    export const showGames = [
+        {
+            gameName: "Stefan Cel Mare Apiary",
+            location: "Suceava, Romania",
+            hives: "50000000000000",
+            bees: "23456789",
+            honey: "76677"
+        },
+        {
+            gameName: "Vlad Tepes Apiary",
+            location: "Targoviste, Romania",
+            hives: "30000000000000",
+            bees: "12345678",
+            honey: "56677"
+        },
+        {
+            gameName: "Mihai Viteazul Apiary",
+            location: "Alba Iulia, Romania",
+            hives: "20000000000000",
+            bees: "34567890",
+            honey: "46677"
+        },
+        {
+            gameName: "Stefan Cel Mititel Apiary",
+            location: "Suceava, Romania",
+            hives: "50000000000000",
+            bees: "23456789",
+            honey: "76677"
+        },
+        {
+            gameName: "Vlad Impaler Apiary",
+            location: "Targoviste, Romania",
+            hives: "30000000000000",
+            bees: "12345678",
+            honey: "56677"
+        },
+        {
+            gameName: "Mihai Cel Fricos Apiary",
+            location: "Alba Iulia, Romania",
+            hives: "20000000000000",
+            bees: "34567890",
+            honey: "46677"
+        },
+        {
+            gameName: "Mihai Cel Fricos Apiary",
+            location: "Alba Iulia, Romania",
+            hives: "20000000000000",
+            bees: "34567890",
+            honey: "46677"
+        }
+    ];
+
+    export const getHives
+    =[{
+        hiveId:"1",
+        bees:"123456",
+        rapeseedHoney:"136.6kg",
+        totalHoney:"316.6kg",
+        date:""
+    },
+    {
+        hiveId:"2",
+        bees:"123456",
+        rapeseedHoney:"136.6kg",
+        totalHoney:"316.6kg"
+    },
+    {
+        hiveId:"3",
+        bees:"123456",
+        rapeseedHoney:"136.6kg",
+        totalHoney:"316.6kg"
+    },
+    {
+        hiveId:"4",
+        bees:"123456",
+        rapeseedHoney:"136.6kg",
+        totalHoney:"316.6kg"
+    },
+    {
+        hiveId:"5",
+        bees:"123456",
+        rapeseedHoney:"136.6kg",
+        totalHoney:"316.6kg"
+    },
+    {
+        hiveId:"6",
+        bees:"123456",
+        rapeseedHoney:"136.6kg",
+        totalHoney:"316.6kg"
+    },
+    {
+        hiveId:"7",
+        bees:"123456",
+        rapeseedHoney:"136.6kg",
+        totalHoney:"316.6kg"
+    },
+    {
+        hiveId:"8",
+        bees:"123456",
+        rapeseedHoney:"136.6kg",
+        totalHoney:"316.6kg"
+    },
+    {
+        hiveId:"9",
+        bees:"123456",
+        rapeseedHoney:"136.6kg",
+        totalHoney:"316.6kg"
+    },
+    {
+        hiveId:"10",
+        bees:"123456",
+        rapeseedHoney:"136.6kg",
+        totalHoney:"316.6kg"
     }]
 
-export const getGames = [
-    {
-        gameName: "Stefan Cel Mare Apiary",
-        location: "Suceava, Romania",
-        hives: "50000000000000",
-        bees: "23456789",
-        honey: "76677"
-    },
-    {
-        gameName: "Vlad Tepes Apiary",
-        location: "Targoviste, Romania",
-        hives: "30000000000000",
-        bees: "12345678",
-        honey: "56677"
-    },
-    {
-        gameName: "Mihai Viteazul Apiary",
-        location: "Alba Iulia, Romania",
-        hives: "20000000000000",
-        bees: "34567890",
-        honey: "46677"
-    },
-    {
-        gameName: "Stefan Cel Mititel Apiary",
-        location: "Suceava, Romania",
-        hives: "50000000000000",
-        bees: "23456789",
-        honey: "76677"
-    },
-    {
-        gameName: "Vlad Impaler Apiary",
-        location: "Targoviste, Romania",
-        hives: "30000000000000",
-        bees: "12345678",
-        honey: "56677"
-    },
-    {
-        gameName: "Mihai Cel Fricos Apiary",
-        location: "Alba Iulia, Romania",
-        hives: "20000000000000",
-        bees: "34567890",
-        honey: "46677"
-    },
-    {
-        gameName: "Mihai Cel Fricos Apiary",
-        location: "Alba Iulia, Romania",
-        hives: "20000000000000",
-        bees: "34567890",
-        honey: "46677"
-    }
-];
-
-export const getHives
-=[{
-    hiveId:"1",
-    bees:"123456",
-    rapeseedHoney:"136.6kg",
-    totalHoney:"316.6kg"
-},
-{
-    hiveId:"2",
-    bees:"123456",
-    rapeseedHoney:"136.6kg",
-     totalHoney:"316.6kg"
-},
-{
-    hiveId:"3",
-    bees:"123456",
-    rapeseedHoney:"136.6kg",
-     totalHoney:"316.6kg"
-},
-{
-    hiveId:"4",
-    bees:"123456",
-    rapeseedHoney:"136.6kg",
-    totalHoney:"316.6kg"
-},
-{
-    hiveId:"5",
-    bees:"123456",
-    rapeseedHoney:"136.6kg",
-     totalHoney:"316.6kg"
-},
-{
-    hiveId:"6",
-    bees:"123456",
-    rapeseedHoney:"136.6kg",
-     totalHoney:"316.6kg"
-},
-{
-    hiveId:"7",
-    bees:"123456",
-    rapeseedHoney:"136.6kg",
-    totalHoney:"316.6kg"
-},
-{
-    hiveId:"8",
-    bees:"123456",
-    rapeseedHoney:"136.6kg",
-     totalHoney:"316.6kg"
-},
-{
-    hiveId:"9",
-    bees:"123456",
-    rapeseedHoney:"136.6kg",
-     totalHoney:"316.6kg"
-},
-{
-    hiveId:"10",
-    bees:"123456",
-    rapeseedHoney:"136.6kg",
-    totalHoney:"316.6kg"
-}]
-
-export const iterateGame
-    = [{
-        gameName: "Dacic Apiary",
-        NextDate: "8-Apr-2024"
-    }]
+    export const iterateGame
+        = [{
+            gameName: "Dacic Apiary",
+            NextDate: "8-Apr-2024"
+        }]
 
 
-export const SellHoney
-    = [{
-        rapeseedHoney: "50",
-        acaciaHoney: "40",
-        lindenHoney: "50.6",
-        wildFlowerHoney: "22.8",
-        sunFlowerHoney: "50.9",
-        falseIndigoFlowerHoney: "50",
-    }]
+    export const SellHoney
+        = [{
+            rapeseedHoney: "50",
+            acaciaHoney: "40",
+            lindenHoney: "50.6",
+            wildFlowerHoney: "22.8",
+            sunFlowerHoney: "50.9",
+            falseIndigoFlowerHoney: "50",
+        }]
