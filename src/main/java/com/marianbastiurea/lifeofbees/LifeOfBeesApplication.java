79eae3442bc84f691ae1912e104dae0280689c56
MarianBastiurea <marian.bastiurea@gmail.com>
2024-12-10 08:56:17 +0000

    add some changes to algorithm

--- src/main/java/com/marianbastiurea/lifeofbees/LifeOfBeesApplication.java
+++ src/main/java/com/marianbastiurea/lifeofbees/LifeOfBeesApplication.java
@@ -1,6 +1,7 @@
 package com.marianbastiurea.lifeofbees;
 
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LifeOfBeesApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load(); // Load the .env file
+        System.setProperty("SPRING_SECURITY_OAUTH2_CLIENT_REGISTRATION_GOOGLE_CLIENT_ID", dotenv.get("SPRING_SECURITY_OAUTH2_CLIENT_REGISTRATION_GOOGLE_CLIENT_ID"));
+        System.setProperty("SPRING_SECURITY_OAUTH2_CLIENT_REGISTRATION_GOOGLE_CLIENT_SECRET", dotenv.get("SPRING_SECURITY_OAUTH2_CLIENT_REGISTRATION_GOOGLE_CLIENT_SECRET"));
+
         SpringApplication.run(LifeOfBeesApplication.class, args);
     }
 
