d2120691f1ffe48f461db9bac5d1f4d98347a56b
MarianBastiurea <marian.bastiurea@gmail.com>
2024-12-10 12:39:43 +0000

    update CreateNewGame

--- frontend/bees-app/src/components/CreateNewGame.js
+++ frontend/bees-app/src/components/CreateNewGame.js
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { createGame, fetchLocations, fetchWeatherForStartDate, getGame} from './BeesApiService';
 
 
-const NewGameModal = ({ handleClose,isPublic,userId,token }) => {
+const NewGameModal = ({ handleClose,isPublic,userId }) => {
     useEffect(() => {
         console.log('Received userId in NewGameModal:', userId);
     }, [userId]);
@@ -32,22 +32,16 @@ const NewGameModal = ({ handleClose,isPublic,userId,token }) => {
             };
     
             console.log('Game data for createNewGame sent:', gameData);
-            const response = await createGame(gameData); // Creează jocul
+            const response = await createGame(gameData); 
             console.log('Răspunsul primit de la createGame:', response);
-    
-            // Extrage gameId și token din răspuns
             const { gameId, token } = response; 
             console.log('ID-ul jocului este:', gameId);
             console.log('Tokenul JWT este:', token);
-    
-            // Obține detaliile jocului
             const gameDetails = await getGame(gameId);
             console.log('Detaliile jocului sunt:', gameDetails);
-    
-            // Navighează către GameView, trecând gameId
             navigate('/GameView', {
                 state: {
-                    gameId: gameId, // Transmite doar ID-ul jocului
+                    gameId: gameId,
                 }
             });
         } catch (error) {
