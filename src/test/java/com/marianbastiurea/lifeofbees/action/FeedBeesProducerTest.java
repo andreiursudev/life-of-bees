a9cdd7146ec98a34505cae10f1496641c638af28
MarianBastiurea <marian.bastiurea@gmail.com>
2025-01-27 07:46:43 +0000

    refactoring FeedBeesProducer

--- src/test/java/com/marianbastiurea/lifeofbees/action/FeedBeesProducerTest.java
+++ src/test/java/com/marianbastiurea/lifeofbees/action/FeedBeesProducerTest.java
@@ -5,14 +5,17 @@ import com.marianbastiurea.lifeofbees.bees.Hive;
 import com.marianbastiurea.lifeofbees.bees.Hives;
 import com.marianbastiurea.lifeofbees.time.BeeTime;
 import org.junit.jupiter.api.Test;
+
 import java.util.List;
 import java.util.Optional;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class FeedBeesProducerTest {
 
     @Test
-    void cantFeedBeesIfMonthIsNotSeptember(){
+    void cantFeedBeesIfMonthIsNotSeptember() {
         Optional<Integer> result = new FeedBeesProducer().produce(
                 new Hives(
                         List.of(
@@ -22,21 +25,22 @@ class FeedBeesProducerTest {
                         new BeeTime(2023, 8, 22)
                 )
         );
-        assertTrue(result.isEmpty(),"Month is not September");
+        assertTrue(result.isEmpty(), "Month is not September");
 
     }
 
     @Test
-    void canFeedBeesIfMonthSeptember(){
-            Optional<Integer> result = new FeedBeesProducer().produce(
-                    new Hives(
-                            List.of(
-                                    new Hive(1, new EggFrames(4, 0.1)),
-                                    new Hive(2, new EggFrames(4, 0.1))
-                            ),
-                            new BeeTime(2023, 9, 22)
-                    )
-            );
-            assertEquals(2, result.orElse(0), "Month is September");
-        }
+    void canFeedBeesIfMonthSeptember() {
+        Optional<Integer> result = new FeedBeesProducer().produce(
+                new Hives(
+                        List.of(
+                                new Hive(1, new EggFrames(4, 0.1)),
+                                new Hive(2, new EggFrames(4, 0.1))
+                        ),
+                        new BeeTime(2023, 9, 22)
+                )
+        );
+        assertTrue(result.isPresent(), "Result should not be empty for September.");
+        assertEquals(2, result.get(), "Month is September");
+    }
 }
