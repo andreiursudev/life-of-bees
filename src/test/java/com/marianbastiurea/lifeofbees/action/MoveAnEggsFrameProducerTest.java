dfadfd3fec42519513f7d75b75b8eda30d0670b9
MarianBastiurea <marian.bastiurea@gmail.com>
2025-01-28 10:58:28 +0000

    add a new test in MoveAnEggsFrameProducer

--- src/test/java/com/marianbastiurea/lifeofbees/action/MoveAnEggsFrameProducerTest.java
+++ src/test/java/com/marianbastiurea/lifeofbees/action/MoveAnEggsFrameProducerTest.java
@@ -111,5 +111,23 @@ class MoveAnEggsFrameProducerTest {
         assertTrue(result.isEmpty());
     }
 
+    @Test
+    void testProduceWithValidSourceAndTwoTargetHives() {
+
+        EggFrames sourceEggFrames = new EggFrames(6, new LinkedList<>(Arrays.asList(6400, 6400, 6400, 6400, 6400, 6400)), false);
+        Hive sourceHive = new Hive(1, sourceEggFrames,false);
+        EggFrames targetEggFrames1 = new EggFrames(5, new LinkedList<>(Arrays.asList(6400, 6400, 6400, 6400, 6400)), false);
+        Hive targetHive1 = new Hive(2, targetEggFrames1,true);
+        EggFrames targetEggFrames2 = new EggFrames(3, new LinkedList<>(Arrays.asList(6400, 6400, 6400)), false);
+        Hive targetHive2 = new Hive(3, targetEggFrames2,true);
+
+        List<Hive> hives = Arrays.asList(sourceHive, targetHive1,targetHive2);
+        Hives hiveCollection = new Hives(hives);
+        MoveAnEggsFrameProducer producer = new MoveAnEggsFrameProducer();
+        Optional<List<List<Integer>>> result = producer.produce(hiveCollection);
+
+        assertEquals(Arrays.asList(1, 2), result.get().get(0));
+        assertEquals(Arrays.asList(1, 3), result.get().get(1));
+    }
 
 }
