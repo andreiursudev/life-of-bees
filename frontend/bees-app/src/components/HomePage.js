469fed065c8fb73005ee57888f3fbd70f878f363
MarianBastiurea <marian.bastiurea@gmail.com>
2025-02-20 06:32:42 +0000

    Update HomePage

--- frontend/bees-app/src/components/HomePage.js
+++ frontend/bees-app/src/components/HomePage.js
@@ -284,7 +284,7 @@ const HomePage = () => {
                     setIsSignUp={setIsSignUp}
                 />
             )}
-            {authMessage && <p className="auth-message">{authMessage}</p>}
+
         </div>
     );
 };
